d316991e5d25237ad6aae3112d1bdae286bbf276
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-08-07 14:16:15 +0530

    Modified self queries

--- gbq_query_builder.py
+++ gbq_query_builder.py
@@ -29,7 +29,7 @@ def generate_base_queries(file_name,output_file):
 
     if not df.empty:
         # Group the DataFrame by the 'Group', 'Source Table', and 'Target Table' columns
-        grouped = df.groupby([GROUP, SOURCE_SCHEMA, SOURCE_TABLE, JOIN_TABLE_1,JOINER_COLUMN_1,JOIN_TYPE1,TARGET_TABLE])
+        grouped = df.groupby([GROUP,SOURCE_PROJECT, SOURCE_SCHEMA, SOURCE_TABLE, JOIN_TABLE_1,JOINER_COLUMN_1,JOIN_TYPE1,TARGET_TABLE])
         """
         print("grouped df:\n")
         for key, item in grouped:
@@ -41,7 +41,7 @@ def generate_base_queries(file_name,output_file):
             source_table_alias='s'
             join_table_alias = 'j'
             # Iterate over each group
-            for (group, source_schema, source_table,join_table_1,join_column1,join_type_1 ,target_table), group_df in grouped:
+            for (group, source_project,source_schema, source_table,join_table_1,join_column1,join_type_1 ,target_table), group_df in grouped:
                 # Check if the source table and target table are the same for all rows in the group
                 if group_df[SOURCE_TABLE].nunique() > 1 or group_df[TARGET_TABLE].nunique() > 1 or group_df[JOIN_TABLE_1].nunique() > 1:
                     print(f"Source Table ,Target Table and JOIN Table are not the same for all rows in group {group}. Skipping.")
@@ -52,7 +52,7 @@ def generate_base_queries(file_name,output_file):
                 target_columns = ', '.join([f"{col}" for col in column_values.split(', ')])
                 if join_type_1 =='L':
                     join_type_1='LEFT OUTER JOIN'
-                query =  f"""SELECT DISTINCT \n{source_columns} \nFROM `{source_schema}.{source_table}`  as {source_table_alias} \n {join_type_1} \n `{source_schema}.{join_table_1}` as {join_table_alias} \n on {source_table_alias}.{join_column1}={join_table_alias}.{join_column1} \n EXCEPT \n DISTINCT \nSELECT {target_columns} \nFROM `{target_table}`;\n"""
+                query =  f"""SELECT DISTINCT \n{source_columns} \nFROM `{source_project}.{source_schema}.{source_table}`  as {source_table_alias} \n {join_type_1} \n `{source_schema}.{join_table_1}` as {join_table_alias} \n on {source_table_alias}.{join_column1}={join_table_alias}.{join_column1} \n EXCEPT \n DISTINCT \nSELECT {target_columns} \nFROM `{target_table}`;\n"""
 
                 # Print the SQL query
                 #print(query)
@@ -62,33 +62,6 @@ def generate_base_queries(file_name,output_file):
     else:
         print('!! No entry found for base queries,hence no queries generated')
     print("generate_base_queries .... completed ")
-
-
-def generate_self_queries_old(file_name, output_file):
-    print("generate_self_queries .... started ")
-    # Read the Excel file
-    df = pd.read_excel(file_name)
-    # Filter the DataFrame based on the 'Source Type' column
-    df = df.loc[df[SOURCE_TYPE] == SOURCE_TYPE_SELF_VALUE]
-    if not df.empty:
-        # Open the output file
-        with open(output_file, 'a') as f:
-            f.write("-- All Self Queries " + '\n')
-            # Iterate over each row
-            for index, row in df.iterrows():
-                target_table = row[TARGET_TABLE]
-                target1 = row[TRANSFORMATION_RULE]
-                target2 = row[TARGET_COLUMN]
-
-                # Form the SQL query
-                query = f"""SELECT {target1}, {target2} , \nCASE WHEN {target1} = {target2} THEN 'False' \nELSE 'True' \nEND \nFROM `{target_table}`;\n"""
-                #print(query)
-                # Write the SQL query to the output file
-                f.write(query + '\n')
-
-    else:
-        print('No entry found for Self queries')
-    print("generate_self_queries .... ended ")
 
 def generate_self_queries(file_name, output_file):
     print("generate_self_queries .... started ")
